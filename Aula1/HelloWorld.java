4a85418db8aac89cf87275d450470b4938aed044
Dandariene <dandariene@hotmail.com>
2022-07-12 20:29:42 -0300

    Exercício da primeira aula de java. Começando do Zero

---
+++ Aula1/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld{
+	public static void main(String[] args){
+		int a = 3;
+		int b = 5;
+		System.out.println("Hello, World!!! " + (a + b));
+	}
+}
---
+++ Aula1/OlaMundo.java
@@ -0,0 +1,8 @@
+class OlaMundo{
+	public static void main(String[] args){
+		int a = 3;
+		int b = 5;
+		System.out.println("Olá, Mundo!!!" );
+		System.out.println("A soma dos dois números é: " + (a + b));
+	}
+}
